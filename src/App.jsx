e136e57ff66f6121215b1cfdedc336f51e739c1a
Marco-Venerando <marco.venera@gmail.com>
2026-02-04 17:16:21 +0100

    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-// bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import * as bootstrap from "bootstrap";
-
 export default function App() {
   return (
     <>
