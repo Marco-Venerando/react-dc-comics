508aa6ce3fbd5db30ff764f62f4bd83ceb07fdc6
Marco-Venerando <marco.venera@gmail.com>
2026-02-05 16:47:25 +0100

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,66 @@
 export default function Header() {
-  return <h1>header</h1>;
+  return (
+    <nav className="navbar navbar-expand bg-body-tertiary fixed-top">
+      <div className="container">
+        {/* LOGO */}
+        <a className="navbar-brand" href="#">
+          <img src="/img/dc-logo.png" alt="logo" height="60" />
+        </a>
+
+        {/* MENU */}
+        <ul className="navbar-nav ms-auto dc-menu">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Characters
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
+              Comics
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Movies
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              TV
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Games
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Collectibles
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Videos
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Fans
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              News
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Shop
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
